79149e9df8f163f54ca4f0fccf8ef3ab7a7fe3d8
H0rangi <formation0451@hotmail.com>
2024-05-28 11:07:32 +0100

    debug

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Greeting from "./components/CodeOne";
+import Counter from "./components/CodeTwo";
+
+function App() {
+  return (
+    <>
+      <Greeting  name="john"/>
+      <Counter />
+    </>
+  );
+}
+
+export default App;
